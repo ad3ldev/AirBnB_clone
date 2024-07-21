41944b184bfeae1ef31fcbd1fdaa4b7810cd2fe8
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2024-07-21 16:22:20 +0300

    baseModel

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""__init__ magic method for models directory"""
-from models.engine.file_storage import FileStorage
+"""
+Create a unique FileStorage instance for your application
+"""
+
+from .engine.file_storage import FileStorage
 
 
 storage = FileStorage()
